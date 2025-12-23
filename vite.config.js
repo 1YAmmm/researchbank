25ce1f968ed093d9667964e2428c6a5ebdf616a4
IYAmmm <superyammy101@gmail.com>
2025-12-23 21:30:52 +0800

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    outDir: "dist/angular", // change output folder to match deployment script
+  },
 });
