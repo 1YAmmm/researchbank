5c6d326756ac69ce16879c2227f11f6f8352f083
IYAmmm <superyammy101@gmail.com>
2025-12-26 10:35:43 +0800

    Describe what you changed

--- api/auth/register.js
+++ api/auth/register.js
@@ -1,4 +1,4 @@
-import { registerUser } from "../../controllers/authController.js";
+import { registerUser } from "../controllers/authController.js";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
